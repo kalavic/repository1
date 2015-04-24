942420527d613b8af31f82bb946d1f1bc027a387
kalav_000 <kalav_000@192.168.0.80>
2015-04-24 17:22:21 -0600

    Merge origin/master
    Conflicts:
    	README.md

---
+++ GalToLit/src/galtolit/GalToLit.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package galtolit;
+
+/**
+ *
+ * @author kalav_000
+ */
+public class GalToLit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double gallons; //holds the number of gallons
+        double liters; // holds conversion to liters
+        
+        gallons = 10; // start with 10 gallons
+        liters = gallons * 3.7854; // convert to liters
+        System.out.println(gallons + " gallons is " + liters + " liters.");
+    }
+    
+}
